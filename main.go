8d20eb2e8c4ebfa876d5094394afea95687356c1
ayeshaq2 <student-00-8c8b1b16127a@qwiklabs.net>
2026-03-19 02:13:41 +0000

    correct error

--- main.go
+++ main.go
@@ -26,7 +26,7 @@ import (
 
 func main() {
 	http.HandleFunc("/blue", blueHandler)
-        http.HandleFunct("/red", redHandler)
+        http.HandleFunc("/red", redHandler)
 	http.ListenAndServe(":8080", nil)
 }
 
